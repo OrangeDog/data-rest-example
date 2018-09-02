193964059e8a1b1fd2798db0e985a55330dd08bf
James <675056+OrangeDog@users.noreply.github.com>
2018-09-02 17:37:58 +0100

    Empty Spring Initializr project with likely dependencies.

---
+++ src/main/java/com/example/worldpay/WorldpayApplication.java
@@ -0,0 +1,12 @@
+package com.example.worldpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldpayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WorldpayApplication.class, args);
+  }
+}
